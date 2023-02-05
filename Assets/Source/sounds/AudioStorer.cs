c620e634ffbd85928a294a4712d1765c5c2e292c
Pablo Cano <pablo0935@gmail.com>
2023-02-04 20:21:28 -0500

    Update new Background

---
+++ Assets/Source/sounds/AudioStorer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class AudioDefinition
+{
+    public AudioClip Clip = null;
+    [Range(0f, 1f)]
+    public float Volume = 1f;
+    public bool Loop = false;
+}
+
+[CreateAssetMenu]
+public class AudioStorer : ScriptableObject
+{
+    public List<AudioDefinition> Audios = new List<AudioDefinition>();
+}
